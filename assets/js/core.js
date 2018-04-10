bdf5f6689e57bf301136834dd9cf1be07307b673
chempa francis <francis.chempa@stu.ucc.edu.gh>
2018-04-10 10:04:16 +0000

    update - no responsiveness yet

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,2 +1,11 @@
 // replace feather icon with svg equivalent ...
 feather.replace();
+
+function hidelogin(){
+    var l = document.getElementsByClassName("log_in_wrap")[0];
+    l.style.marginTop="-300px";
+}
+function showlogin(){
+    var l = document.getElementsByClassName("log_in_wrap")[0];
+    l.style.marginTop="0";
+}
