098f083d57e0a471a07e965f8d5abfd82291ecb7
chempa francis <francis.chempa@stu.ucc.edu.gh>
2018-04-13 20:00:05 +0000

    Responsiveness

--- assets/js/core.js
+++ assets/js/core.js
@@ -4,8 +4,22 @@ feather.replace();
 function hidelogin(){
     var l = document.getElementsByClassName("log_in_wrap")[0];
     l.style.marginTop="-300px";
+    hide_mob_sidenav();
 }
 function showlogin(){
     var l = document.getElementsByClassName("log_in_wrap")[0];
     l.style.marginTop="0";
+    hide_mob_sidenav();
+}
+function show_mob_login(){
+    if(document.getElementById("login_toogle").innerHTML=="0"){
+        document.getElementsByClassName("mob_login")[0].style.marginTop="0px";
+        document.getElementById("login_toogle").innerHTML="1";
+    }else{
+        document.getElementsByClassName("mob_login")[0].style.marginTop="-300px";
+        document.getElementById("login_toogle").innerHTML="0";
+    }
+}
+function hide_mob_sidenav(){
+    document.getElementsByClassName("mob_login")[0].style.marginTop="-300px";
 }
